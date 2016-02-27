70c947cdd67c4fe318b2b00ee0f905e43f09e09b
hiluluke <hiluluke@hotmail.com>
2016-02-27 23:49:15 +0800

    demo

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,24 @@ gulp.task('ds-js',function() {
     .pipe(gulp.dest("./dist/js/"));
 });
 
+gulp.task('ss-js',function() {
+
+    gulp.src("./src/js/*.js")
+    .pipe(rename(function (path) {
+        path.basename += "-debug";
+    }))
+    .pipe(gulp.dest("./"))
+    .pipe(jshint())
+    .pipe(ugnify().on('error',function(e) {
+        console.error(e.message, e.lineNumber);
+        this.emit('end');
+    }))
+    .pipe(rename(function (path) {
+        path.basename = path.basename.split('-debug')[0];
+    }))
+    .pipe(gulp.dest("./"));
+});
+
 gulp.task('ds-css',function() {
     gulp.src('./src/css/*.less')
     .pipe(less().on('error',function(e) {
